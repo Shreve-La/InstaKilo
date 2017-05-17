2c4c5d4f14c4fc9bbff5cd452a97f16a4c023dad
Shreve-La <shrevela@gmail.com>
2017-05-17 12:53:16 -0400

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by swcl on 2017-05-17.
+//  Copyright © 2017 Shreve LaFramenta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InstaKilo
+//
+//  Created by swcl on 2017-05-17.
+//  Copyright © 2017 Shreve LaFramenta. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
